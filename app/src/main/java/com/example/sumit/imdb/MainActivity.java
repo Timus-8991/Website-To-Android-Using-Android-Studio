22b4a1f1452014ef620c4d9f30a520fb12286769
sumit saini <sainisumit935@yahoo.com>
2017-10-11 20:45:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sumit/imdb/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sumit.imdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebview = (WebView) findViewById(R.id.web_view);
+         WebSettings webSettings = mWebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(false);
+        mWebview.loadUrl("http://www.imdb.com/");
+        mWebview.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebview.canGoBack())
+        {
+            mWebview.goBack();
+        }else
+        super.onBackPressed();
+    }
+}
